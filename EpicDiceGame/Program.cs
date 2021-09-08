4177e4b5a22b64a94841c23343a6171565fd4790
Opilane TTHK <opilane@tthk.ee>
2021-09-08 12:16:09 +0300

    RandomNumberGenerators

---
+++ EpicDiceGame/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace EpicDiceGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //mängus osaleb kaks mängijat;
+            //iga mängija viskab täringut (1-6);
+            //programm kuvab nende visete tulemused ja kuvab ka, kes on võitja
+            // - ehk kes viskab suuremat
+
+            Random rnd = new Random();
+
+            string esimängija = "Vladimir Putin";
+
+            int plr1 = rnd.Next(1, 6);
+
+            Random rnd2 = new Random();
+
+            string teinemängija = "Donald Trump";
+
+            int plr2 = rnd.Next(1, 6);
+
+            Console.WriteLine(esimängija + " viskab taringuga numbri " + plr1);
+            Console.WriteLine(teinemängija + " viskab taringuga numbri " + plr2);
+
+            if (plr1 < plr2)
+            {
+                Console.WriteLine(esimängija + " on võitja");
+            }
+            else if (plr1 > plr2)
+            {
+                Console.WriteLine(teinemängija + " on võitja");
+            }
+            else
+            {
+                Console.WriteLine("viik");
+            }
+        }
+    }
+}
